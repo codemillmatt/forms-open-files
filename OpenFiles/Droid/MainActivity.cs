e5b942ea2df7e1eaaacf34d0204d76dda3207f03
Matt Soucoup <msoucoup@codemilltech.com>
2017-02-16 17:07:52 -0600

    getting droid to work

--- OpenFiles/Droid/MainActivity.cs
+++ OpenFiles/Droid/MainActivity.cs
@@ -11,6 +11,7 @@ using Android.OS;
 namespace OpenFiles.Droid
 {
 	[Activity(Label = "OpenFiles.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[IntentFilter(new[] { Intent.ActionSend }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = @"application/pdf")]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
@@ -20,9 +21,19 @@ namespace OpenFiles.Droid
 
 			base.OnCreate(bundle);
 
+			if (Intent.Action == Intent.ActionSend)
+			{
+				var clip = Intent.ClipData;
+				var pdf = clip.GetItemAt(0);
+
+				Console.Write("hi");
+			}
+
 			global::Xamarin.Forms.Forms.Init(this, bundle);
 
 			LoadApplication(new App());
 		}
+
+
 	}
 }
---
+++ OpenFiles/Droid/PDF/PDFWebViewRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using OpenFiles;
+using OpenFiles.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(PDFWebView), typeof(PDFWebViewRenderer))]
+namespace OpenFiles.Droid
+{
+	public class PDFWebViewRenderer : WebViewRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+				var customWebView = Element as PDFWebView;
+				Control.Settings.AllowUniversalAccessFromFileURLs = true;
+				Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}", string.Format("file:///android_asset/Content/{0}", WebUtility.UrlEncode(customWebView.Uri))));
+			}
+		}
+	}
+}
